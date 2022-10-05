0c550fecf4bf5ae86278f5943b2782fe65afc85d
smartin38 <smartin38@fordham.edu>
2022-10-05 09:58:50 -0400

    react log-in

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -10,10 +10,9 @@ export function LoginForm() {
             <div className={`${styles.contactContainer} container`}>
                 <div className={styles.contactSideBySide}>
                     <div className={styles.contactMainPart}>
-                        <h3 className={styles.contactTitle}><a href="https://open.spotify.com/track/58zsLZPvfflaiIbNWoA22O?si=3a358c16f55e42ac">I'm only human after all</a></h3>
+                        <h3 className={styles.contactTitle}><a href="https://open.spotify.com/track/58zsLZPvfflaiIbNWoA22O?si=3a358c16f55e42ac">Welcome back!</a></h3>
                         <div className={styles.contactLimitContainer}>
                             <p className={styles.contactDetails}>
-                                Yes, you read my text right. If you have a question, please fill out the form below and I will get back to you as soon as possible.
                             </p>
                         </div>
 
@@ -21,27 +20,20 @@ export function LoginForm() {
                             <div className={styles.contactGrid}>
                                 <form onSubmit={handleSubmit}>
                                     <label htmlFor="name">
-                                        Name
+                                        Username
                                     </label>
                                     <input className={styles.messageInput}
-                                        id="name"
-                                        type="name"
-                                        name="name"
+                                        id="username"
+                                        type="username"
+                                        name="username"
                                     />
                                     <label htmlFor="email">
-                                        Email Address
+                                        Password
                                     </label>
                                     <input className={styles.messageInput}
-                                        id="email"
-                                        type="email"
-                                        name="email"
-                                    />
-                                    <label htmlFor="message">
-                                        Message
-                                    </label>
-                                    <textarea className={styles.messageInput}
-                                        id="message"
-                                        name="message"
+                                        id="password"
+                                        type="password"
+                                        name="password"
                                     />
                                     <button className={styles.thisButton} type="submit">
                                         Submit
